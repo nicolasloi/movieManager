263458f0da53db899fb0499f14c5d1e844d1ca19
nicolasloison13 <nicolasloison13@icloud.com>
2023-03-28 16:51:20 +0200

    change style for create.blade.php

--- resources/views/movies/create.blade.php
+++ resources/views/movies/create.blade.php
@@ -1,45 +1,44 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Add New Movie</h1>
+    <div class="h-screen bg-secondary">
+        <h1 class="ml-10 text-xl">Add new Movie</h1>
 
-    {!! Form::open(['route' => 'movies.store', 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
-    <div class="form-group">
-        {{Form::label('title', 'Title')}}
-        {{Form::text('title', '', ['class' => 'form-control', 'placeholder' => 'Title'])}}
-    </div>
-    <div class="form-group p-6">
-        {{Form::file('cover_image', ['class' => 'file-input file-input-bordered file-input-primary w-full max-w-xs'])}}
-    </div>
-    <div class="form-group">
-        {{Form::label('body', 'Body')}}
-        {{Form::textarea('body', '', ['id' => 'article-ckeditor', 'class' => 'form-control', 'placeholder' => 'Body Text'])}}
-    </div>
-    <div class="form-group">
-        {{ Form::label('star_rating', 'Rating') }}
-        @php
-            $ratings = [1, 2, 3, 4, 5];
-        @endphp
+        <div class="bg-white rounded-2xl shadow-lg p-6 mt-10 mx-auto max-w-lg">
+            {!! Form::open(['route' => 'movies.store', 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
+            <div class="mb-4">
+                {{Form::label('title', 'Title', ['class' => 'block text-gray-700 font-bold mb-2'])}}
+                {{Form::text('title', '', ['class' => 'input input-bordered w-full py-2 px-3', 'placeholder' => 'Title'])}}
+            </div>
+            <div class="mb-4">
+                {{Form::file('cover_image', ['class' => 'file-input file-input-bordered file-input-primary'])}}
+            </div>
+            <div class="mb-4">
+                {{Form::label('body', 'Body', ['class' => 'block text-gray-700 font-bold mb-2'])}}
+                {{Form::textarea('body', '', ['id' => 'article-ckeditor', 'class' => 'input input-bordered w-full py-2 px-3', 'placeholder' => 'Body Text'])}}
+            </div>
+            <div class="mb-4">
+                {{ Form::label('star_rating', 'Rating', ['class' => 'block text-gray-700 font-bold mb-2']) }}
+                @php
+                    $ratings = [1, 2, 3, 4, 5];
+                @endphp
 
-        {{ Form::label('star_rating', 'Rating') }}
-        <div class="rating">
-            @for ($i = 0; $i < count($ratings); $i++)
-                @if ($ratings[$i] == 1)
-                    {{ Form::radio('star_rating', $ratings[$i], true, ['id' => 'star' . $ratings[$i], 'class' => 'mask mask-star-2 bg-green-500']) }}
-                @else
-                    {{ Form::radio('star_rating', $ratings[$i], false, ['id' => 'star' . $ratings[$i], 'class' => 'mask mask-star-2 bg-green-500']) }}
-                @endif
-                <label for="star{{ $ratings[$i] }}"></label>
-            @endfor
+                <div class="rating flex items-center">
+                    @for ($i = 0; $i < count($ratings); $i++)
+                        @if ($ratings[$i] == 1)
+                            {{ Form::radio('star_rating', $ratings[$i], true, ['id' => 'star' . $ratings[$i], 'class' => 'mask mask-star-2 bg-yellow-400 mr-1']) }}
+                        @else
+                            {{ Form::radio('star_rating', $ratings[$i], false, ['id' => 'star' . $ratings[$i], 'class' => 'mask mask-star-2 bg-yellow-400 mr-1']) }}
+                        @endif
+                        <label for="star{{ $ratings[$i] }}"></label>
+                    @endfor
+                </div>
+            </div>
+            <div class="flex items-center justify-between">
+                {{Form::submit('Submit', ['class'=>'bg-primary text-white font-bold py-2 px-4 rounded'])}}
+                {!! Form::close() !!}
+                <a href="/dashboard" class="btn btn-primary text-white">Cancel</a>
+            </div>
         </div>
     </div>
-    <div>
-    {{Form::submit('Submit', ['class'=>'btn btn-primary text-white'])}}
-    {!! Form::close() !!}
-
-        <a href="/dashboard" class="btn btn-primary text-white">Cancel</a>
-
-    </div>
 @endsection
-
-
