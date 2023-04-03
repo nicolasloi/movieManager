470c934bbd0ab2c6697081cc74ca9b2aeea9f54c
nicolasloison13 <nicolasloison13@icloud.com>
2023-04-03 16:51:22 +0200

    edit style in home

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,21 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1 class="text-xl font-bold mb-4">Trending movies</h1>
-    <div class="flex flex-wrap -mx-4">
-        @foreach($trending_movies as $movie)
-            <div class="w-full md:w-1/2 lg:w-1/6 p-4">
-                <div class="bg-white rounded-lg shadow-lg">
-                    <img src="https://image.tmdb.org/t/p/w500{{ $movie['poster_path'] }}" class="w-full rounded-t-lg"
-                         alt="{{ $movie['title'] }} poster">
-                    <div class="p-4">
-                        <h5 class="text-lg font-bold mb-2">{{ $movie['title'] }}</h5>
-                        <p class="text-gray-700 text-base leading-relaxed mb-4">{{ $movie['overview'] }}</p>
-                        <a href="#" class="btn btn-primary">Details</a>
+    <div class="h-screen bg-secondary">
+        <h1 class="ml-11 text-xl">Trending movies</h1>
+        <div class="flex w-max p-5 gap-5 overflow-x-scroll">
+            @foreach($trending_movies as $trending_movie)
+                <div class="w-full">
+                    <div class="bg-white rounded-lg shadow-lg">
+                        <img src="https://image.tmdb.org/t/p/w500{{ $trending_movie['poster_path'] }}"
+                             class="w-full rounded-t-lg"
+                             alt="{{ $trending_movie['title'] }} poster">
+                        <div class="p-4">
+                            <h5 class="text-lg font-bold mb-2">{{ $trending_movie['title'] }}</h5>
+                        </div>
                     </div>
                 </div>
-            </div>
-        @endforeach
+            @endforeach
+        </div>
     </div>
-
 @endsection
